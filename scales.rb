6efeb644e3317167ce6ba97d2d2535cdbc9d52d0
Jose Treviño <i1f54142@outlook.com>
2024-10-15 18:38:06 -0600

    removes redundant functionalityfrom scales

--- scales.rb
+++ scales.rb
@@ -13,13 +13,16 @@ define_scale_from_example "PentatonicMinor", %w(e g a b d)
 define_scale_from_example "PentatonicMajor", %w(e g# a# b c#)
 define_scale_from_example "SpanishPhrygian", %w(a a# c# d e f g)
 define_scale_from_example "Phrygian", %w(a a# c d e f g)
-define_scale_from_example "Blues", %w(e g a a# b d)
+define_scale_from_example "BluesMajor", %w(c d d# e g a)
+define_scale_from_example "BluesMinor", %w(c d# f f# g a#)
+define_scale_from_example "Diminished", %w(c d d# f f# g# a b)
 define_scale_from_example "SuspendedPentatonic", %w(c d f g a#)
 define_scale_from_example "Sakura", %w(c c# f g g#)
 define_scale_from_example "Insen", %w(d d# g a c)
 define_scale_from_example "Iwato", %w(c c# f f# a#)
 define_scale_from_example "Japanese", %w(a b c e f)
 define_scale_from_example "Yo", %w(d e g a b)
+
 
 def all_scales
     flags = {}
